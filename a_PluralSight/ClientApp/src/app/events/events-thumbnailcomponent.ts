dad4dca5d2545a8b239390f8137ebaa5dd894717
Amber Jones <amberlovescats14@gmail.com>
2020-03-09 20:45:40 -0500

    first commit

---
+++ a_PluralSight/ClientApp/src/app/events/events-thumbnailcomponent.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: 'events-thumbnail',
+  template: `<div>
+    <h1>Upcoming Angular Events </h1>
+    <hr>
+    <h2>{{event.name}}</h2>
+</div> `
+
+})
+
+export class EventsThumbnailComponent {
+
+  @Input() event: any;
+
+}
